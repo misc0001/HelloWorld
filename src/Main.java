16e190983f71d944509583eee3232cb587562adb
misc0001 <misc0001@stud.kea.dk>
2023-09-08 14:55:04 +0200

    Added "Hello Dat23c!"

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Dat23c!");
+        System.out.println("1234");
     }
 }
